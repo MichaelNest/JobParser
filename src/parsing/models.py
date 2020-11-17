7b714693c4113b92faee517a3138e5dcf2e07270
MichaelNest <nesterenkom385@gmail.com>
2020-11-17 18:36:02 +0200

    Module 2 path 20 - create file utils.py and method autocreate slug.

--- src/parsing/models.py
+++ src/parsing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .utils import *
 
 class City(models.Model):
     name = models.CharField(max_length=55, verbose_name='Название города', unique=True)
@@ -7,17 +8,28 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyr_to_lat(str(self.name))
+        super().save(*args, **kwargs)
+
+
     class Meta:
         verbose_name='Город'
         verbose_name_plural = 'Города'
-        
+
 class Language(models.Model):
     name = models.CharField(max_length=55, unique=True, verbose_name='Язык программирования')
     slug = models.CharField(max_length=55, blank=True, unique=True)
-    
+
     def __str__(self):
         return self.name
-    
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyr_to_lat(str(self.name))
+        super().save(*args, **kwargs)
+
     class Meta:
         verbose_name='Язык программирования'
         verbose_name_plural= 'Языки программирования'
